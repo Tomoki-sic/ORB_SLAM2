92d70b7c4a02340bb7147823b2f26711d2ab8958
Tomoki-sic <ma21085@shibaura-it.ac.jp>
2022-12-21 18:07:51 +0900

    add "GrabImageMonocularWAF"

--- include/Tracking.h
+++ include/Tracking.h
@@ -61,6 +61,8 @@ public:
     cv::Mat GrabImageStereo(const cv::Mat &imRectLeft,const cv::Mat &imRectRight, const double &timestamp);
     cv::Mat GrabImageRGBD(const cv::Mat &imRGB,const cv::Mat &imD, const double &timestamp);
     cv::Mat GrabImageMonocular(const cv::Mat &im, const double &timestamp);
+    cv::Mat GrabImageMonocularWAF(const cv::Mat &im, const cv::Mat &im_middle, const cv::Mat &im_high, const double &timestamp);
+
 
     void SetLocalMapper(LocalMapping* pLocalMapper);
     void SetLoopClosing(LoopClosing* pLoopClosing);
